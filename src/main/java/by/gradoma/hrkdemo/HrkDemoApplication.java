a13b4b04c748b2d7fb0a81a5c893f5fb389e277b
Gradoma <gradomski25@gmail.com>
2021-02-27 21:01:36 +0300

    init commit

---
+++ src/main/java/by/gradoma/hrkdemo/HrkDemoApplication.java
@@ -0,0 +1,13 @@
+package by.gradoma.hrkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrkDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/gradoma/hrkdemo/HrkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.gradoma.hrkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
